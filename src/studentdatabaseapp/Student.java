7f8cd6c030e91cb939f0421c938e42b626b0f239
Aquib A <saquiba@ciena.com>
2022-03-14 22:14:00 +0530

    added studentDBapp class

--- src/studentdatabaseapp/Student.java
+++ src/studentdatabaseapp/Student.java
@@ -1,2 +1,37 @@
-package studentdatabaseapp;public class Student {
+package studentdatabaseapp;
+
+import java.util.Scanner;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private int gradeYear;
+    private int studentID;
+    private String courser;
+    private String tutionBalance;
+    private int costOfCourse = 600;
+
+    // Constructor: prompt user to enter student's name and year
+    public Student(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter student first name: ");
+        this.firstName = sc.nextLine();
+
+        System.out.println("Enter student last name: ");
+        this.lastName = sc.nextLine();
+
+        System.out.println("1 - Frehsmen\n2 - Sophmore\n3 - Junior\n4 - Senior\nEnter student class level: ");
+        this.gradeYear = sc.nextInt();
+        System.out.println(firstName + " " + lastName + " Gread = " + gradeYear);
+    }
+    // Generate an ID
+
+    // Enroll in courses
+
+    // View balance
+
+    // Pay tution fee
+
+    // Show status
+
 }
--- src/studentdatabaseapp/studentDatabaseApp.java
+++ src/studentdatabaseapp/studentDatabaseApp.java
@@ -1,2 +1,10 @@
-package studentdatabaseapp;public class studentDatabaseApp {
+package studentdatabaseapp;
+
+public class studentDatabaseApp {
+    public static void main(String[] args) {
+        // Ask how many new users we want to add
+        Student st = new Student();
+
+        // Create n number of new students
+    }
 }
